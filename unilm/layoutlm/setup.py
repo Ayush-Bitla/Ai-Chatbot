2f5249423bfdea11469e06d8bd4b758cff043969
Abhishek Pal <128469413+Abhi951197@users.noreply.github.com>
2024-09-19 21:46:19 +0530

    Update setup.py

--- unilm/layoutlm/setup.py
+++ unilm/layoutlm/setup.py
@@ -14,9 +14,9 @@ setup(
     packages=find_packages(exclude=("configs", "tests")),
     python_requires=">=3.6",
     install_requires=[
-        "transformers==2.9.0",
+        "transformers==4.44.2",
         "tensorboardX==2.0",
-        "lxml==4.5.1",
+        "lxml==4.9.1",
         "seqeval==0.0.12",
         "Pillow==7.1.2",
     ],
